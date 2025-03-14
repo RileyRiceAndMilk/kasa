34063d6a2828268c0adaede3fd6d1837f71cee9f
RileyRiceAndMilk <167301632+RileyRiceAndMilk@users.noreply.github.com>
2025-03-14 16:35:44 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import logo from './assets/logo.PNG';
 
+const logo = '/assets/logo.PNG'; 
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    {/* Affichage de l'image logo dans index.jsx */}
+    <img src={logo} alt="Logo" />
   </React.StrictMode>
 );
 
