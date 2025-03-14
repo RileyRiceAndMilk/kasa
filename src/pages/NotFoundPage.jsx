f2afd4f2a862fb6a5f04cc661a47ed2cc204a50b
RileyRiceAndMilk <167301632+RileyRiceAndMilk@users.noreply.github.com>
2025-03-14 16:52:56 +0100

    Update NotFoundPage.jsx

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import "../css/style.css";
 
 const NotFoundPage = () => {
   return (
-    <div>
-      <h1>Page non trouvée - 404</h1>
-    </div>
+    <body>
+      <header>
+
+        <Link to="/" className="logo-link">
+          <img src="src/assets/logo.png" className="logo" alt="Logo Kasa" />
+        </Link>
+
+
+        <nav className="link-container">
+          <Link to="/Acceuil" className="link-Acceuil">Accueil</Link>
+          <Link to="/" className="link-A-propos">À propos</Link>
+        </nav>
+      </header>
+      <main>
+        <h1>404</h1>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+        <Link to="/Acceuil" className="link-Acceuil">Retourner sur la page d'acceuil</Link>
+      </main>
+      <footer>
+        <img src="src/assets/logokasa.png" className="logo" alt="Logo Kasa" />
+        <p>2020 Kasa. All rights reserved.</p>
+      </footer>
+    </body>
   );
 };
 
